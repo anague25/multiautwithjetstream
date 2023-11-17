4a0cb33a0b0099f466d3edbd958df5baa4cc2c74
anague sonna roosvelt <anaguesonnaroosvelt@gmail.com>
2023-11-17 08:58:12 +0100

    login

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -2,9 +2,10 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 use App\Models\Admin;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 
 /**
@@ -21,9 +22,9 @@ class AdminFactory extends Factory
     {
         return [
             'name' => "Admin",
-            'email' => "admin@gmail.com",
+            'email' => "admin3@gmail.com",
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('password'), // password
             'remember_token' => Str::random(10),
             'profile_photo_path' => null,
             'current_team_id' => null,
